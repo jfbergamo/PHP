ad759c3778443bc228457071f70436927e67380d
J <jacopobergamasco06@gmail.com>
2025-02-19 10:53:57 +0100

    Esercizio corretto godo ho preso 9

--- negozio2/editor/inserisci.php
+++ negozio2/editor/inserisci.php
@@ -10,15 +10,6 @@ $idModello = $_POST['modello'] ?? '';
 $prezzo = $_POST['prezzo'] ?? '';
 $agente = $_POST['agente'] ?? '';
 
-// CREATE TABLE `vendite` (
-//     `idVendita` int(11) NOT NULL,
-//     `cliente` int(11) DEFAULT NULL,
-//     `dataVendita` date DEFAULT NULL,
-//     `modello` int(11) DEFAULT NULL,
-//     `prezzo` decimal(7,2) DEFAULT NULL,
-//     `agente` varchar(20) DEFAULT NULL
-//   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 $query = "INSERT INTO vendite VALUES (NULL, $idCliente, DATE('$dataVendita'), $idModello, $prezzo, '$agente')";
 $db->query($query);
 
